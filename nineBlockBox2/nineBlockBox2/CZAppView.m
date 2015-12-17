75a58f299aeab50838338ad3ed8b720450eb8d98
jinjinwang <297913156@qq.com>
2015-12-17 16:59:53 +0800

    增加按钮点击事件

--- nineBlockBox2/nineBlockBox2/CZAppView.m
+++ nineBlockBox2/nineBlockBox2/CZAppView.m
@@ -15,7 +15,7 @@
 @property (weak, nonatomic) IBOutlet UIButton *downloadBtn;
 @property (weak, nonatomic) IBOutlet UILabel *appName;
 @property (weak, nonatomic) IBOutlet UIImageView *appIcon;
-- (IBAction)download:(id)sender;
+- (IBAction)download:(UIButton *)sender;
 @end
 
 @implementation CZAppView
@@ -45,11 +45,61 @@
  *
  *  @param sender <#sender description#>
  */
-- (IBAction)download:(id)sender {
+- (IBAction)download:(UIButton *)sender {
     // 初始化alertview
-    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"下载成功" message:nil delegate:self cancelButtonTitle:@"确定" otherButtonTitles:nil];
+    // UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"下载成功" message:nil delegate:self cancelButtonTitle:@"确定" otherButtonTitles:nil];
     //显示AlertView
-    [alert show];
+    // [alert show];
     NSLog(@"点击了下载按钮");
+    // 1. 禁用当前按钮
+    sender.enabled = NO;
+    
+    // 2. 创建一个消息提示框
+    UILabel *label = [[UILabel alloc] init];
+    // 2.1 设置文字
+    label.text = @"正在下载...";
+    // 2.2 设置背景色
+    label.backgroundColor = [UIColor blackColor];
+    CGFloat lbWith = 200;
+    CGFloat lbHeight = 100;
+    CGFloat y = (self.superview.frame.size.height - lbHeight) / 2;
+    CGFloat x = (self.superview.frame.size.width - lbWith) / 2;
+    // 2.3 设置位置
+    label.frame = CGRectMake(x, y, lbWith, lbHeight);
+    // 2.4 设置字体颜色
+    label.textColor = [UIColor redColor];
+    // 2.5 设置居中
+    label.textAlignment = NSTextAlignmentCenter;
+    // 2.6 设置字体大小和粗体
+    label.font = [UIFont boldSystemFontOfSize:17];
+    // 2.7 设置透明度
+    label.alpha = 0.0;
+    // 2.8 设置圆角
+    // 设置四个角的半径
+    label.layer.cornerRadius= 10;
+    // 把多余的部分剪掉
+    label.layer.masksToBounds = YES;
+    
+    
+    // 2.9 通过动画的方式来显示label
+    [UIView animateWithDuration:1.5 animations:^{
+        label.alpha = 0.7;
+    } completion:^(BOOL finished) {
+        if(finished){
+            // 隔一段时间再启动另外一个动画
+            [UIView animateKeyframesWithDuration:1.5 delay:1 options:UIViewAnimationCurveLinear animations:^{
+                label.alpha = 0.0;
+            } completion:^(BOOL finished) {
+                if(finished){
+                    [label removeFromSuperview];
+                }
+            }];
+            
+        }
+    }];
+    
+    
+    [self.superview addSubview:label];
+    
 }
 @end
